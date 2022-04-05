856f9f45cea7e803a170207578eba1c4d87e2fab
Nam Dang <nam.dang@saleshood.com>
2022-04-06 01:22:00 +0700

    new Change

--- renamefile.py
+++ renamefile.py
@@ -1,11 +1,33 @@
+import mimetypes
 import magic
 import os
 import sys
 import getopt
 
+def get_extension(filePath):
+  return "."+mime.from_file(filePath).split("/")[1]
+
+
 path = sys.argv[1]
-print(path)
+baseName = str(path.split("/")[-1])+"_"
+mime = magic.Magic(mime=True)
+
+
 for filename in os.listdir(path):
-  f = os.path.join(path,filename)
-  newPath = os.Path.splitext(f)[0]
-  os.rename(f,newPath)
+  filename = filename.split("_")[-1]
+  oldFile = os.path.join(path,filename)
+  ext = get_extension(oldFile)
+
+  newName = baseName+filename.split(".")[0]+ext
+  newFile = os.path.join(path,newName)
+
+  os.rename(oldFile,newFile)
+
+# for filename in os.listdir(path):
+#   newName = filename.split("_")[-1]
+#   oldFile = os.path.join(path,filename)
+#   newFile = os.path.join(path,newName)
+
+#   os.rename(oldFile,newFile)
+
+
